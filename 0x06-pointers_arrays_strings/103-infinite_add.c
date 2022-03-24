19ba253ae66df4d7601f30bf4484de802eb93e4f
oEbuka <ebukaobiora10@gmail.com>
2022-03-24 07:03:20 -0700

    3rd commit

--- 0x06-pointers_arrays_strings/103-infinite_add.c
+++ 0x06-pointers_arrays_strings/103-infinite_add.c
@@ -1,24 +1,23 @@
-                     val1 = *(n1 + i) - '0';
-                if (j < 0)
-                        val2 = 0;
-                else
-                        val2 = *(n2 + j) - '0';
-                temp_tot = val1 + val2 + overflow;
-                if (temp_tot >= 10)
-                        overflow = 1;
-                else
-                        overflow = 0;
-                if (digits >= (size_r - 1))
-                        return (0);
-                *(r + digits) = (temp_tot % 10) + '0';
-                digits++;
-                j--;
-                i--;
-        }
-        if (digits == size_r)
-                return (0);
-        *(r + digits) = '\0';
-        rev_string(r);
-        return (r);
-}
+#include <stdio.h>
 
+int main(void)
+{
+  int n;
+  int a[5];
+  int *p;
+
+  a[2] = 1024;
+  p = &n;
+  /*
+   * write your line of code here...
+   * Remember:
+   * - you are not allowed to use a
+   * - you are not allowed to modify p
+   * - only one statement
+   * - you are not allowed to code anything else than this line of code
+   */
+ p[5] = 98;
+  /* ...so that this prints 98\n */
+  printf("a[2] = %d\n", a[2]);
+  return (0);
+}
